4d380fbc9daee087745cd52832f1141180572af9
Mathew Mariani <mat.mariani@hotmail.com>
2025-05-26 22:04:09 -0400

    Updated navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,58 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-light">
+  <!-- Fixed top navbar -->
+  <nav class="navbar bg-light fixed-top shadow-sm">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/">{{ siteTitle }}</a>
-
+      <!-- Offcanvas toggle button -->
       <button
         class="navbar-toggler"
         type="button"
-        data-bs-toggle="collapse"
+        data-bs-toggle="offcanvas"
         data-bs-target="#navbarNav"
         aria-controls="navbarNav"
-        aria-expanded="false"
         aria-label="Toggle navigation"
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav" ref="navbarCollapse">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item" v-for="(link, index) in navLinks" :key="index">
-            <a class="nav-link" :href="link.href">
-              {{ link.label }}
-            </a>
-          </li>
-        </ul>
+
+      <!-- Brand -->
+      <a class="navbar-brand" href="/">{{ siteTitle }}</a>
+
+      <!-- Offcanvas menu -->
+      <div
+        class="offcanvas offcanvas-start"
+        tabindex="-1"
+        id="navbarNav"
+        aria-labelledby="navbarNavLabel"
+      >
+        <div
+          class="offcanvas-header"
+          style="position: sticky; top: 0; background-color: white; z-index: 1"
+        >
+          <h5 class="offcanvas-title" id="navbarNavLabel">{{ siteTitle }}</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item" v-for="(link, index) in navLinks" :key="index">
+              <h1><a class="nav-link" :href="link.href">{{ link.label }}</a></h1>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </nav>
 </template>
 
 <script setup>
-import { ref, onMounted, nextTick } from 'vue';
-import { Collapse } from 'bootstrap';
+import { onMounted } from 'vue';
+import { Offcanvas } from 'bootstrap';
 
+// Props
 defineProps({
   siteTitle: {
     type: String,
@@ -38,6 +60,7 @@ defineProps({
   },
 });
 
+// Navigation links
 const navLinks = [
   { label: 'Clubs', href: '/clubs' },
   { label: 'Fixtures', href: '/schedule' },
@@ -46,4 +69,17 @@ const navLinks = [
   { label: 'Settings', href: '/settings' },
 ];
 
+// Manually initialize offcanvas to ensure it works after navigation
+onMounted(() => {
+  const offcanvasEl = document.getElementById('navbarNav');
+  if (offcanvasEl) {
+    new Offcanvas(offcanvasEl);
+  }
+});
 </script>
+
+<style scoped>
+.offcanvas {
+  max-width: 75%;
+}
+</style>
