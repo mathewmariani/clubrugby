0e5a8b053cc6417fd4bb6f6991b9994d066f5268
Mathew Mariani <mat.mariani@hotmail.com>
2025-06-09 18:57:57 -0400

    Prettier

--- src/components/vue/tabs/DetailsTab.vue
+++ src/components/vue/tabs/DetailsTab.vue
@@ -4,7 +4,13 @@
       <div class="list-group-item">
         <!-- header home -->
         <div class="d-flex gap-3 mb-3 my-3">
-          <img :src="club1?.logo_url" alt="" width="64" height="64" style="object-fit: contain" />
+          <img
+            :src="club1?.logo_url"
+            alt=""
+            width="64"
+            height="64"
+            style="object-fit: contain"
+          />
           <div class="d-flex flex-column justify-content-center">
             <strong>{{ club1.name }}</strong>
             <p class="text-muted mb-0">{{ team1Record }}</p>
@@ -12,7 +18,13 @@
         </div>
         <!-- header away -->
         <div class="d-flex gap-3 mb-3 my-3">
-          <img :src="club2?.logo_url" alt="" width="64" height="64" style="object-fit: contain" />
+          <img
+            :src="club2?.logo_url"
+            alt=""
+            width="64"
+            height="64"
+            style="object-fit: contain"
+          />
           <div class="d-flex flex-column justify-content-center">
             <strong>{{ club2.name }}</strong>
             <p class="text-muted mb-0">{{ team2Record }}</p>
@@ -24,14 +36,26 @@
       <div class="list-group-item">
         <div class="d-flex justify-content-between my-3">
           <div class="d-flex flex-column justify-content-center">
-            <img :src="club1?.logo_url" alt="" width="32" height="32" style="object-fit: contain" />
+            <img
+              :src="club1?.logo_url"
+              alt=""
+              width="32"
+              height="32"
+              style="object-fit: contain"
+            />
           </div>
           <div class="d-flex flex-column justify-content-center text-center">
             <strong>Record</strong>
             <p class="text-muted mb-0">TOTAL</p>
           </div>
           <div class="d-flex flex-column justify-content-center">
-            <img :src="club2?.logo_url" alt="" width="32" height="32" style="object-fit: contain" />
+            <img
+              :src="club2?.logo_url"
+              alt=""
+              width="32"
+              height="32"
+              style="object-fit: contain"
+            />
           </div>
         </div>
         <div v-for="stat in regular_stats" :key="stat.key" class="mb-3">
