627ad0cf04de8d8ba7dece3886655225836c9305
mathewmariani <someone.mariani@gmail.com>
2026-02-01 10:39:34 -0500

    Re-added symlinks to test build pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { symlinkIntegration } from './src/integrations/symlink';
 
 import vue from '@astrojs/vue';
 import path from 'path';
@@ -7,7 +8,7 @@ import path from 'path';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://clubrugby.ca',
-  integrations: [vue()],
+  integrations: [vue(), symlinkIntegration()],
   vite: {
       resolve: {
           preserveSymlinks: true,
