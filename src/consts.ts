84837a011ea6b758c5dec09fd8dcdcfd285fa29a
mathewmariani <someone.mariani@gmail.com>
2026-02-15 11:38:52 -0500

    Removed uneeded consts

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,4 @@ export const SITE_TITLE = 'clubrugby.ca';
 export const SITE_DESCRIPTION =
   'In-depth stats & matchup coverage for Canadian rugby unions.';
 
-export const SYMLINK_SRC = 'data';
-export const SYMLINK_DST = 'src/data';
-
 export const CONTACT_EMAIL = 'clubrugbyca@gmail.com';
